f6c9626c9daf4cf325aa07af4ff00f51ef742320
Serhii Petrash <serhiipetrash@gmail.com>
2022-02-23 15:02:58 +0200

    add LoginForm + SignupForm

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <button>Log in</button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const email = ref('')
+const password = ref('')
+
+const handleSubmit = () => {
+  console.log(email.value, password.value);
+}
+
+</script>
+
+<style>
+</style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" required placeholder="display name" v-model="displayName" />
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <button>Sign up</button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const displayName = ref('')
+const email = ref('')
+const password = ref('')
+
+const handleSubmit = () => {
+  console.log(displayName.value, email.value, password.value);
+}
+
+</script>
+
+<style>
+</style>
