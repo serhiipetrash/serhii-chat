d223e4e0c90fc3baa501d6765507101a90b5e4ae
Serhii Petrash <serhiipetrash@gmail.com>
2022-02-23 15:25:35 +0200

    switching between Forms

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="welcome container">
     <p>Welcome</p>
-    <SignupForm />
-    <LoginForm />
+    <div v-if="showLogin">
+      <h2>Login</h2>
+      <LoginForm />
+      <p>
+        No account yet?
+        <span @click="showLogin = false">Signup</span> instead
+      </p>
+    </div>
+    <div v-else>
+      <h2>Sign up</h2>
+      <SignupForm />
+      <p>
+        Already registered?
+        <span @click="showLogin = true">login</span> instead
+      </p>
+    </div>
   </div>
 </template>
 
 <script setup>
 import SignupForm from '../components/SignupForm.vue';
 import LoginForm from '../components/LoginForm.vue';
+import { ref } from 'vue';
+
+const showLogin = ref(true)
 
 
 
@@ -36,5 +53,13 @@ import LoginForm from '../components/LoginForm.vue';
   outline: none;
   color: #999;
   margin: 10px auto;
+}
+.welcome span {
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
+}
+.welcome button {
+  margin: 20px auto;
 }
 </style>
