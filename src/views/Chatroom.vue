cb0f882a5ee00aa96e066766fc7f22a7ae27dab1
Serhii Petrash <serhiipetrash@gmail.com>
2022-02-24 12:12:41 +0200

    add Navbar

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
-    <p>Chatroom</p>
+  <div class="container">
+    <Navbar />
   </div>
 </template>
 
 <script setup>
+import Navbar from '../components/Navbar.vue';
 
 </script>
 
