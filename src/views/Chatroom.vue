a437d40f9f12d64bf0759cac4afcf7bf889d0bfd
Serhii Petrash <serhiipetrash@gmail.com>
2022-02-24 15:27:54 +0200

    watching current user 115

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,7 +5,19 @@
 </template>
 
 <script setup>
+import { watch } from 'vue';
+import { useRouter } from 'vue-router';
 import Navbar from '../components/Navbar.vue';
+import getUser from '../composables/getUser';
+
+const { user } = getUser()
+const router = useRouter()
+
+watch(user, () => {
+  if (!user.value) {
+    router.push({ name: 'Welcome' })
+  }
+})
 
 </script>
 
