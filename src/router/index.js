fbc40eae37a4cfb5a7e22ef4b7a2fba9aa00b753
Serhii Petrash <serhiipetrash@gmail.com>
2022-02-24 15:10:26 +0200

    add route guards 113

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Welcome from '../views/Welcome.vue';
 import Chatroom from '../views/Chatroom.vue';
+import { projectAuth } from '../firebase/config';
+
+// auth guards
+const requireAuth = (to, from, next) => {
+  let user = projectAuth.currentUser;
+  console.log('current user in auth guard: ', user);
+  if (!user) {
+    next({ name: 'Welcome' });
+  } else {
+    next();
+  }
+};
 
 const routes = [
   { path: '/', name: 'Welcome', component: Welcome },
-  { path: '/chatroom', name: 'Chatroom', component: Chatroom },
+  {
+    path: '/chatroom',
+    name: 'Chatroom',
+    component: Chatroom,
+    beforeEnter: requireAuth,
+  },
 ];
 
 const router = createRouter({
